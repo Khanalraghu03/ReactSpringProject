143e538a6ce6f082beb91bb4d53803e78bfaa788
Raghu Khanal <khanalraghu03@gmail.com>
2020-05-16 16:31:58 -0400

    initial commit

---
+++ src/main/java/com/reactspring/model/DemoLoader.java
@@ -0,0 +1,20 @@
+package com.reactspring.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoLoader implements CommandLineRunner{
+    private final ContactRepository repository;
+
+    @Autowired
+    public DemoLoader(ContactRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.repository.save(new Contact("John", "Doe", "jdoe@email.com"));
+    }
+}
